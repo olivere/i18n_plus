24c830a8a645aaffa27f026a98e1978cb7350b6a
Oliver Eilhard <oliver@eilhard.net>
2015-07-21 11:53:07 +0200

    Fix issue with minitest

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,11 @@
 require 'rubygems'
 require 'bundler/setup'
-require 'minitest/unit'
+require 'minitest/autorun'
 require 'active_support'
 require 'action_view'
 require 'mocha/setup'
 require 'i18n_plus'
-MiniTest::Unit.autorun
+MiniTest.autorun
 
 # Do not enfore available locales
 I18n.enforce_available_locales = false
